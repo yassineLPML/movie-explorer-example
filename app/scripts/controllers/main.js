e15e38b3955c6edc1869743e78ffd6e791e4d7b0
yassineLPML <kaouachi12.yassine@gmail.com>
2016-12-11 19:58:29 +0100

    Update main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,9 +12,67 @@ angular.module('movieExplorerApp')
     var ctrl = this;
 
     ctrl.movies = [];
+	ctrl.slides = [];
 
     movies.getDiscoverMovies().then(function(result)
     {
       ctrl.movies = result;
+	  
+	  angular.forEach(ctrl.movies, function(movie)
+	  {
+		  if(movie.backdrop_path)
+		  {
+			  movies.getImageURL(movie.backdrop_path,'w780').then(function(imageUrl)
+			  {
+				  ctrl.slides.push(imageUrl);
+			  });
+		  }
+	  });
+	  
+	  ctrl.currentSlide = 0;
     });
+	
+	ctrl.getNextSlideIndex = function()
+	{
+		if(ctrl.currentSlide === ctrl.slides.length -1)
+		{
+			return 0;
+		}
+		return ctrl.currentSlide + 1;
+	};
+	
+	ctrl.getPreviousSlideIndex = function()
+	{
+		if(ctrl.currentSlide === 0)
+		{
+			return ctrl.slides.length -1;
+		}
+		return ctrl.currentSlide - 1;
+	};
+	
+	ctrl.slideIsVisible = function(index)
+	{
+		return index === ctrl.currentSlide || 
+		index === ctrl.getPreviousSlideIndex() || 
+		index === ctrl.getNextSlideIndex();
+	};
+	
+	ctrl.getSlideClass = function(index)
+	{
+		if(index === ctrl.currentSlide)
+		{
+			return 'current';
+		}else if(index === ctrl.getPreviousSlideIndex()){
+			return 'previous';
+		}else if(index === ctrl.getNextSlideIndex()){
+			return 'next';
+		}
+		return '';
+	};
+	
+	ctrl.gotoSlide = function(index)
+	{
+		ctrl.currentSlide = index;
+	};
+	
   });
