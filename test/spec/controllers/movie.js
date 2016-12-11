e63f85c3df1b3f6a0e13c9481436ad6566356e41
yassineLPML <kaouachi12.yassine@gmail.com>
2016-12-11 20:11:49 +0100

    Update movie.js

--- test/spec/controllers/movie.js
+++ test/spec/controllers/movie.js
@@ -17,7 +17,4 @@ describe('Controller: MovieCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MovieCtrl.awesomeThings.length).toBe(3);
-  });
 });
